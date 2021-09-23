341839a33fc0a5bc53874ad7bf3d93068d3356e7
Rytis Cepulis <cepRytis@gmail.com>
2021-09-23 23:42:07 +0300

    validate phone length

--- ValidatorLibrary/PhoneValidator.cs
+++ ValidatorLibrary/PhoneValidator.cs
@@ -2,9 +2,29 @@
 {
     public class PhoneValidator
     {
+        private readonly int _requiredLenForWithoutPrefix = 8;
+        private readonly int _lenLongPrefix = 4;
+        private readonly int _lenShortPrefix = 1;
+
         public bool Validate(string phone)
         {
+            if (!IsValidLength(phone))
+            {
+                return false;
+            }
+
             return false;
+        }
+
+        private bool IsValidLength(string phone)
+        {
+            if (_requiredLenForWithoutPrefix + _lenLongPrefix != phone.Length
+                || _requiredLenForWithoutPrefix + _lenShortPrefix != phone.Length)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
