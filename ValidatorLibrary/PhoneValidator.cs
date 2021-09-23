5db0b7852a95eedfe39238c578f28d2f6eff3fcc
Rytis Cepulis <cepRytis@gmail.com>
2021-09-24 00:09:08 +0300

    ability to change from 8 to +370

--- ValidatorLibrary/PhoneValidator.cs
+++ ValidatorLibrary/PhoneValidator.cs
@@ -1,10 +1,15 @@
-﻿namespace ValidatorLibrary
+﻿using System;
+
+namespace ValidatorLibrary
 {
     public class PhoneValidator
     {
         private readonly int _requiredLenForWithoutPrefix = 8;
         private readonly int _lenLongPrefix = 4;
         private readonly int _lenShortPrefix = 1;
+
+        private readonly string _shortPrefix = "8";
+        private readonly string _longPrefix = "+370";
 
         public bool Validate(string phone)
         {
@@ -14,6 +19,29 @@
                 return false;
             }
             return true;
+        }
+
+        public string ChangeShortcutPrefixToDefault(string phone)
+        {
+
+            if (!Validate(phone)
+                || phone[0].Equals(_shortPrefix))
+            {
+                return null;
+            }
+
+            var (_, number) = SeperatePrefix(phone);
+            return String.Concat(_longPrefix, number);
+        }
+
+        private (string, string) SeperatePrefix(string phone)
+        {
+            if ('+' == phone[0])
+            {
+                return (phone.Substring(0, 4), phone[4..]);
+            }
+
+            return (phone.Substring(0, 1), phone[1..]);
         }
 
         private bool IsValidLength(string phone)
