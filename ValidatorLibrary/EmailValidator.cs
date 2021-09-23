56379b61c5c72f90658fb204b7d9319a0baa7ae2
Rytis Cepulis <cepRytis@gmail.com>
2021-09-23 23:10:10 +0300

    seperate username domain tld

--- ValidatorLibrary/EmailValidator.cs
+++ ValidatorLibrary/EmailValidator.cs
@@ -4,7 +4,18 @@
     {
         public bool Validate(string email)
         {
-            return false;
+            var atSymbolIndex = email.LastIndexOf('@');
+            var dotSymbolIndex = email.LastIndexOf('.');
+            if (-1 == atSymbolIndex || -1 == dotSymbolIndex)
+            {
+                return false;
+            }
+
+            var username = email.Substring(0, atSymbolIndex);
+            var domain = email.Substring(atSymbolIndex + 1, dotSymbolIndex - atSymbolIndex - 1);
+            var tld = email[(dotSymbolIndex + 1)..];
+
+            return true;
         }
     }
 }
